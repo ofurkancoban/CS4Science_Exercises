842e3a62c564f0aa6d1321ff1a9e66aff84e50a1
ofurkancoban <f.coban93@gmail.com>
2025-04-09 14:08:35 +0200

    Initial

---
+++ CS4Science_Exercises/SoSe25/Task 1-2.py
@@ -0,0 +1,26 @@
+a=4
+b=2
+a = b
+a = (a - b) * b + a
+
+print(a)
+print(b)
+
+c = 6 + 37
+
+a = 2
+type(a)
+
+a = float(a)
+
+2.5
+
+2 5
+
+a = 'Always use the assignment operator '=' for storing values in variables'
+
+a = "1,a"
+
+type(a)
+
+a = "Always use the assignment operator "=" for storing values in variables"
---
+++ CS4Science_Exercises/SoSe25/animalnoise.py
@@ -0,0 +1,21 @@
+# Noisy Animals
+# Alipour, Jalil
+# Coban, Omer Furkan
+# SoSe25
+# CS4Science - Team B2
+
+
+user_input = input("Enter your animal's name: ")
+
+if user_input  == "dog":
+    print("Wuff!")
+elif user_input == "cat":
+    print("Meow!")
+elif user_input == "bird":
+    print("Piep!")
+elif user_input == "cow":
+    print("Moo!")
+elif user_input == "sheep":
+    print("Mee!")
+else:
+    print("Sorry, that's not an animal")
